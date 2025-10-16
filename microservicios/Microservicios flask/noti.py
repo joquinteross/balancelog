46a079664e6e961f177d04a15dea8aa3c1b15c83
joquinteros <joquinteros@unal.edu.co>
2025-10-15 22:36:14 -0500

    feat: microservicio de notificaciones el cual notifica cuando estamos proximos a pasar los laimites

---
+++ microservicios/Microservicios flask/noti.py
@@ -0,0 +1,71 @@
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
+#Endpoints de microservicios a conectar
+presupuestos_url = "http://localhost:5001"
+transacciones_url = "http://localhost:5000"
+
+
+alerta = 0.8
+
+
+@app.route("/")
+def indice():
+    return "ON FUNCIONANDO"
+
+
+#Consulta de estado de un usuario
+@app.route("/notificaciones/<user_id>/<mes>", methods=["GET"])
+def noti_user(user_id, mes):
+
+#Revisamos el presupuesto del usuario
+    respuesta_p = requests.get(f"{presupuestos_url}/presupuesto/{user_id}")
+    if respuesta_p.status_code != 200:
+        return jsonify({"mensaje": "Error al encontrar el resultado"}), 502
+
+    presupuesto = respuesta_p.json()
+    limite = float(presupuesto.get("gasto", 0))
+
+    if limite <= 0:
+        return jsonify({
+            "user_id": user_id,
+            "mes": mes,
+            "estado": "No Limite",
+            "mensaje": "No esta configurado el limite"
+        })
+
+#Revisamos el total de gastos del mes 
+    respuesta_t = requests.get(f"{transacciones_url}/transacciones/resumen/{user_id}/{mes}")
+    if respuesta_t.status_code != 200:
+        return jsonify({"mensaje": "Error al encontrar el resultado"}), 502
+
+    resumen = respuesta_t.json()
+    totalg = float(resumen.get("totalg", 0))
+
+#Realizamos la comparacion de gastos con limites para determinar 
+    ratio = totalg / limite
+    if totalg > limite:
+        estado = "EXCEDIDO"
+        mensaje = f"Te pasaste del limite: {totalg:.2f} / {limite:.2f}"
+    elif ratio >= alerta:
+        estado = "ALERTA"
+        mensaje = f"Ya llevas el {ratio*100:.0f}% de tu límite: {totalg:.2f} / {limite:.2f}"
+    else:
+        estado = "OK"
+        mensaje = f"Estas bien pero no te relajes: {totalg:.2f} / {limite:.2f}"
+
+#Respuesta del endpoint
+    return jsonify({
+        "user_id": user_id,
+        "mes": mes,
+        "total_gastos": totalg,
+        "limite": limite,
+        "estado": estado,
+        "mensaje": mensaje
+    })
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5003)
