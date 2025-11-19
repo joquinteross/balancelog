48376f1312a201a8ba33fbf0bf69ce255db95f6a
joquinteros <joquinteros@unal.edu.co>
2025-11-19 00:39:46 -0500

    feat: creacion de rutas para microservicio de creacion de usuarios

--- microservicios/api-gateway/routes/api.php
+++ microservicios/api-gateway/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsuariosGatewayController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+#Ruta del microservicio de usuarios
+
+
+Route::post('/auth/register', [UsuariosGatewayController::class, 'register']);
+Route::post('/auth/login', [UsuariosGatewayController::class, 'login']);
+Route::post('/auth/logout', [UsuariosGatewayController::class, 'logout']);
+Route::post('/auth/password', [UsuariosGatewayController::class, 'changePassword']);
+Route::get('/auth/me', [UsuariosGatewayController::class, 'me']);
+
